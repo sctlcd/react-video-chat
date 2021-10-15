0ce7fb9fce839c63cdeb14463e1ebf9d4f468e5e
8 <lucinda.souchet@outlook.com>
2021-10-15 23:49:55 +0200

    feat: setup state dor username, roomName, token

--- src/VideoChat.js
+++ src/VideoChat.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const VideoChat = () => {
+    const [username, setUsername] = useState('');
+    const [roomName, setRoomName] = useState('');
+    const [token, setToken] = useState(null);
+
     <div></div> // To be build later
 };
 
