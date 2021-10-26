4af890cba8c85d3318fea893912b41c34dba7192
8 <lucinda.souchet@outlook.com>
2021-10-26 23:49:14 +0200

    feat: add handleSubmit

--- src/VideoChat.js
+++ src/VideoChat.js
@@ -1,20 +1,34 @@
 import React, { useState, useCallback } from 'react';
 
 const VideoChat = () => {
-    const [username, setUsername] = useState('');
-    const [roomName, setRoomName] = useState('');
-    const [token, setToken] = useState(null);
+  const [username, setUsername] = useState('');
+  const [roomName, setRoomName] = useState('');
+  const [token, setToken] = useState(null);
 
-    const handleUsernameChange = useCallback(event => {
-        setUsername(event.target.value);
-    }, []);
+  const handleUsernameChange = useCallback(event => {
+    setUsername(event.target.value);
+  }, []);
 
-    const handleRoomNameChange = useCallback(event => {
-        setRoomName(event.target.value);
-    }, []);
+  const handleRoomNameChange = useCallback(event => {
+    setRoomName(event.target.value);
+  }, []);
 
+  const handleSubmit = useCallback(async event => {
+    event.preventDefault();
+    const data = await fetch('/video/token', {
+      method: 'POST',
+      body: JSON.stringify({
+        identity: username,
+        room: roomName
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json());
+    setToken(data.token);
+  }, [username, roomName]);
 
-    <div></div> // To be build later
+  <div></div> // To be build later
 };
 
 export default VideoChat;
